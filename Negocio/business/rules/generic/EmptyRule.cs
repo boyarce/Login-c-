9d36c2ee962a7a9563791256bf48a47124e390c2
boyarce <b.nelson@alumnos.duoc.cl>
2018-08-17 20:15:38 -0300

    re-volviendo a subir

--- Negocio/business/rules/generic/EmptyRule.cs
+++ Negocio/business/rules/generic/EmptyRule.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Negocio.business.exception;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Negocio.business.rules.generic
 {
-    class EmptyRule
+    public class EmptyRule
     {
+        public void isEmpty(string nameElement, string value)
+        {
+            if (string.IsNullOrEmpty(value)){
+                throw new BusinessException("EL Campo "+nameElement+" no puede estar vacio");
+            }
+        }
     }
 }
